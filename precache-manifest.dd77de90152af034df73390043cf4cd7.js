9ce63ba438771cc183d999447bdd37fc93f5d152
Mehmet Emin Batkitar <49765648+mehmeteminbatkitar@users.noreply.github.com>
2021-08-23 14:50:31 +0300

    Add files via upload

---
+++ precache-manifest.dd77de90152af034df73390043cf4cd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38a2b58df330c85e0029eecf71d7c26",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "49553d6b70419f82c204",
+    "url": "/css/app.7424f7ba.css"
+  },
+  {
+    "revision": "d523c344ce53a084dabd",
+    "url": "/css/chunk-vendors.d43b6a07.css"
+  },
+  {
+    "revision": "e981336c732aaedff14e1b81e1edc4c4",
+    "url": "/img/su.e981336c.svg"
+  },
+  {
+    "revision": "810905bf3ac2bc2f02c5d3cdf056ab40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49553d6b70419f82c204",
+    "url": "/js/app.0251b8c1.js"
+  },
+  {
+    "revision": "d523c344ce53a084dabd",
+    "url": "/js/chunk-vendors.b008b848.js"
+  },
+  {
+    "revision": "af404263581e95681fb5eef2a9a869d8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
